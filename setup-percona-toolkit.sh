883de99cfcbb4d3600006f02f357b0558bc02a90
Roberto Graham <robertogrhm@gmail.com>
2025-02-28 19:03:02 +0000

    :wrench: Download compressed Percona Toolkit tarball.

--- setup-percona-toolkit.sh
+++ setup-percona-toolkit.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Installing..."
+echo "Downloading compressed Percona Toolkit tarball for Product Version $PRODUCT_VERSION..."
+
+curl --silent --show-error --output percona-toolkit.tar.gz https://downloads.percona.com/downloads/percona-toolkit/"$PRODUCT_VERSION"/binary/tarball/percona-toolkit-"$PRODUCT_VERSION"_x86_64.tar.gz
+
+echo "Download complete."
