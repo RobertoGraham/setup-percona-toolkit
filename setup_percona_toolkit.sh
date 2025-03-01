f4ad54196369cc3e3b344f4fb839992e3a5dcc50
Roberto Graham <robertogrhm@gmail.com>
2025-03-01 01:24:52 +0000

    :wrench: Debug.

--- setup_percona_toolkit.sh
+++ setup_percona_toolkit.sh
@@ -3,7 +3,7 @@
 readonly COMPRESSED_PERCONA_TOOLKIT_TARBALL_FILE='percona-toolkit.tar.gz'
 
 download_compressed_percona_toolkit_tarball() {
-  local product_version="$2"
+  local product_version="$1"
   echo 'Downloading compressed Percona Toolkit tarball...'
   curl --silent --show-error --output $COMPRESSED_PERCONA_TOOLKIT_TARBALL_FILE https://downloads.percona.com/downloads/percona-toolkit/"$product_version"/binary/tarball/percona-toolkit-"$product_version"_x86_64.tar.gz
   echo 'Download complete.'
